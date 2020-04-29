73c8e053f628a478d930d122bb196134803fee3f
emchoko <lozev.emil.05@gmail.com>
2020-04-30 00:49:56 +0100

    added ignore for the vscode folder

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
-var assert = require('assert');
+var describe = require('mocha').describe
+var it = require('mocha').it
+var assert = require('assert')
 
 describe('Array', () => {
-    describe('#indexOf', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
+  describe('#indexOf', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1)
+    })
+  })
+})
