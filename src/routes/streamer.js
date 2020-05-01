b0ab2652f30edf553fa67301d15837267722d547
emchoko <lozev.emil.05@gmail.com>
2020-05-01 22:45:04 +0100

    testing the fetch of stream by user id

--- src/routes/streamer.js
+++ src/routes/streamer.js
@@ -1,4 +1,7 @@
 'use strict'
+const waterfall = require('async/waterfall')
+const fetcher = require('../utils/fetcher')
+const StreamerResponse = require('../model/streamer-response.js')
 
 module.exports = (router) => {
   const fetchIndex = (req, res) => {
@@ -6,7 +9,55 @@ module.exports = (router) => {
   }
 
   const fetchStreamer = (req, res) => {
-    return res.send('The id is ' + req.params.id)
+    let userId = req.params.id
+
+    waterfall(
+      [
+        // fetch the stream by user_id
+        (done) => {
+          fetcher
+            .getStreamerById(userId)
+            .then((streamer) => {
+              streamer.json().then((body) => {
+                if (body.status === 200) {
+                  // this means that the user is streaming
+                  if (body.data.length > 0) {
+                    // done(null, body.data[0].game_id)
+                    let resBody = StreamerResponse(true, null)
+                    res.status(200).json(JSON.parse(resBody))
+                    done(null)
+                  } else {
+                    let resBody = StreamerResponse(false, null)
+                    res.status(200).json(JSON.parse(resBody))
+                    done(null)
+                  }
+                } else {
+                  return res.status(body.status).json({
+                    message: 'There was a problem fetching the Twitch API'
+                  })
+                }
+              })
+            })
+            .catch((err) => {
+              console.error(err)
+              done({
+                code: 500,
+                body: {
+                  message: 'An error occurred during the fetch from Twitch',
+                  err: err
+                }
+              })
+            })
+        }
+        // (gameId, done) => {}
+      ],
+      (err) => {
+        if (err) {
+          return res.status(err.code).json(err.body)
+        }
+        res.end()
+      }
+    )
   }
 
   router.get('/', fetchIndex)
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,20 +1,22 @@
 const fetch = require('node-fetch')
+require('dotenv').config()
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Client-ID': process.env.CLIENT_ID
+}
 
 module.exports = {
-  private headers: {
-    'Content-Type': 'application/json',
-    'Client-ID': process.env.CLIENT_ID
-  },
   getStreamerById: (userId) => {
-    return fetch(process.env.API_URL + "streams/?user_id=" + userId, {
-      method: "GET",
-      headers: this.headers
+    return fetch(process.env.API_URL + `streams/?user_id=${userId}`, {
+      method: 'GET',
+      headers: headers
     })
   },
   getGameById: (gameId) => {
-    return fetch(process.env.API_URL + "games/?id=" + gameId, {
-      method: "GET",
-      headers: this.headers
-    });
+    return fetch(process.env.API_URL + `games/?id=${gameId}`, {
+      method: 'GET',
+      headers: headers
+    })
   }
 }
