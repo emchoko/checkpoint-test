4edd51aac4171439c36b692cd6f3da134e4efcf3
emchoko <lozev.emil.05@gmail.com>
2020-05-01 19:01:32 +0100

    added json body parsing as default

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const routes = require('./routes/index')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World, Me Fe!')
-})
+routes(app)
 
 app.listen(3000, () => {
   console.log('App runs on port 9000:3000!')
--- src/routes/streamer.js
+++ src/routes/streamer.js
@@ -1,7 +1,9 @@
 'use strict'
 
 module.exports = (router) => {
-  const fetchIndex = (req, res) => {}
+  const fetchIndex = (req, res) => {
+    return res.send('Documentation here')
+  }
 
   const fetchStreamer = (req, res) => {}
 
